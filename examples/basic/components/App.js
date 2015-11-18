6e0831339ea730ad2d2879b7658e0c6ec4404ae8
James Long <longster@gmail.com>
2015-11-18 00:10:32 -0500

    add a basic example

---
+++ examples/basic/components/App.js
@@ -0,0 +1,31 @@
+const React = require('react');
+const { Link } = require('react-router');
+const { connect } = require('react-redux');
+const { increase, decrease } = require('../actions/count');
+
+const App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <header>
+          Links:
+          {' '}
+          <Link to="/foo">Foo</Link>
+          {' '}
+          <Link to="/bar">Bar</Link>
+        </header>
+        {this.props.children}
+        <div>
+          {this.props.number}
+          <button onClick={() => this.props.increase(1)}>Increase</button>
+          <button onClick={() => this.props.decrease(1)}>Decrease</button>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = connect(
+  state => ({ number: state.count.number }),
+  { increase, decrease }
+)(App);
